0a04aef999b1bc9ad5ec377ab2ecda5d7798ec47
ahmadhammoudy <ahmadhammoudy@gmail.com>
2019-09-23 19:23:30 +0300

    new package for FXML and Controller Signed-off-by: ahmadhammoudy <ahmadhammoudy@gmail.com>

--- FXML/Mk_array_projectController.java
+++ FXML/Mk_array_projectController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.mkarrayproject;
 
 import com.jfoenix.controls.JFXTextArea;
@@ -13,16 +8,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.HBox;
 
-/**
- * FXML Controller class
- *
- * @author HP
- */
 public class Mk_array_projectController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
     
     @FXML
     HBox hbox;
